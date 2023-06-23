fbbd1ba56906591afdd8861c14b608b5d929ab45
Tomás Weigenast <tomaswegenast@gmail.com>
2023-06-23 10:28:29 -0300

    finish

--- ClaseHilos/Program.cs
+++ ClaseHilos/Program.cs
@@ -16,7 +16,7 @@
 //_5Mutex.Excecute();
 
 //6- Uso de lock
-_6lock.Excecute();
+//_6lock.Excecute();
 
-
+Solution.Execute();
 
--- ClaseHilos/solution.cs
+++ ClaseHilos/solution.cs
@@ -9,11 +9,11 @@ namespace ClaseHilos
         public Producto(string nombre, decimal precioUnitario, int cantidadEnStock)
         {
             Nombre = nombre;
-            PrecioUnitario = precioUnitario;
+            PrecioUnitarioDolares = precioUnitario;
             CantidadEnStock = cantidadEnStock;
         }
     }
-    internal class Solution //reference: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/lock
+    internal class Solution
     {
 
         static List<Producto> productos = new List<Producto>
@@ -26,23 +26,83 @@ namespace ClaseHilos
         };
 
         static int precio_dolar = 500;
+        static Mutex precioMutex = new();
+        static Barrier informesBarrier = new(4);
+        static SemaphoreSlim semaphore = new(1, 1);
 
         static void Tarea1()
         {
-            throw new NotImplementedException();
+            semaphore.Wait();
+            foreach(var product in productos)
+            {
+                product.CantidadEnStock += 10;
+            }
+            semaphore.Release();
+            Console.WriteLine("stock actualizado");
+            informesBarrier.SignalAndWait();
         }
         static void Tarea2()
         {
-            throw new NotImplementedException();
+            precioMutex.WaitOne();
+            precio_dolar = new Random().Next(450, 550);
+            precioMutex.ReleaseMutex();
+            Console.WriteLine($"dolar actualizado a AR$ {precio_dolar:C}");
+            informesBarrier.SignalAndWait();
         }
         static void Tarea3()
         {
-            throw new NotImplementedException();
+            informesBarrier.SignalAndWait();
+            Console.WriteLine("::::Informes::::");
+            decimal total = 0;
+
+            Tabla.PrintLine();
+            Tabla.PrintRow("Producto", "Stock", "Precio AR$");
+            Tabla.PrintLine();
+            foreach (var product in productos)
+            {
+                decimal precioArs = product.PrecioUnitarioDolares * precio_dolar;
+                total += precioArs * product.CantidadEnStock;
+                Tabla.PrintRow(product.Nombre, product.CantidadEnStock.ToString(), precioArs.ToString("C"));
+            }
+            Tabla.PrintLine();
+
+            Console.WriteLine($"---- Total: {total:C}");
+
+        }
+
+        static void Tarea4()
+        {
+            semaphore.Wait();
+            foreach (var product in productos)
+            {
+                product.PrecioUnitarioDolares += product.PrecioUnitarioDolares * 0.1m;
+            }
+            semaphore.Release();
+            Console.WriteLine("precios actualizados");
+            informesBarrier.SignalAndWait();
+        }
+
+        static void Timeout()
+        {
+            informesBarrier.SignalAndWait(10000);
+            throw new Exception("Alguna tarea no se ejecutó, terminando...");
         }
         
-        internal static void Excecute ()
+        internal static void Execute ()
         {
-            throw new NotImplementedException();
+            var tarea1 = new Thread(new ThreadStart(Tarea1));
+            var tarea2 = new Thread(new ThreadStart(Tarea2));
+            var tarea3 = new Thread(new ThreadStart(Tarea3));
+            var tarea4 = new Thread(new ThreadStart(Tarea4));
+            var timeout = new Thread(new ThreadStart(Timeout));
+
+            //timeout.Start();
+            tarea3.Start();
+            tarea4.Start();
+            tarea2.Start();
+            tarea1.Start();
+
+            Console.WriteLine("hello floppa, this is StockManager by BigNodes");
         }
     }
 }
